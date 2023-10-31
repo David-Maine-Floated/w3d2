aec753c2ba44390967985eab87a8933ccabca947
David <dmaine6711@gmail.com>
2023-10-31 14:05:26 -0400

    2nd commit, initiate card variable and dfine card methods

---
+++ p1/practice-for-ch-ruby-memory-long-practice-main/card.rb
@@ -0,0 +1,31 @@
+class Card 
+    attr_reader :face_down, :face_value
+    def intialize(face_value) 
+        @face_value = face_value
+        @face_down = true 
+        @default_value = " "
+    end    
+
+    def card_information 
+        if self.face_down 
+            puts 'card is face down.' 
+            self.default_value
+        else  
+            self.face_value    
+        end  
+    end   
+
+    def hide 
+        if !self.face_down
+            self.face_down = true 
+        end    
+    end
+
+    def reveal 
+        if self.face_down
+            puts self.face_value
+            self.face_down = false 
+        end    
+    end    
+
+end   
