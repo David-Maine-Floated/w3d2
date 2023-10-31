afb4d7d21b4f3377ee29236855c863471335c0ca
David <dmaine6711@gmail.com>
2023-10-31 16:02:53 -0400

    commit 5, more bullshit

--- p1/practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ p1/practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -1,4 +1,5 @@
 require "byebug"
+require_relative 'card'
 
 #we probably want to populate when we call the board
 
@@ -6,23 +7,27 @@ class Board
 
   attr_reader :grid
 
-  # @@elements = ("a".."h").to_a
+  @@elements = ("a".."h").to_a
 
   def initialize
     @grid = Array.new(4) { Array.new(4, nil) }
-    @size = @grid.length / 2
+    @size = @grid.length * @grid.length
+    self.populate
     #@grid should take in cards as an arg
   end
   #populate board with @board,size / 2 , end up with 2 of each card 
 
   def populate
+    # debugger  
     new_arr = @@elements + @@elements
-    
+    p new_arr 
     while new_arr.length > 0
+      # debugger 
       i_one = rand(0...@grid.length)
       i_two = rand(0...@grid.length)
       
       if @grid[i_one][i_two] == nil
+        # debugger 
         curr_el = new_arr.pop
         @grid[i_one][i_two] = Card.new(curr_el)
       end
@@ -31,7 +36,10 @@ class Board
   end
 
   def print
-    puts @grid.each {|row| puts row.join(" ")}
+    puts @grid.each do |row|
+      row_to_puts = ""
+      row.each {|space| row_to_puts += space.shown_value}  
+    end  
   end
 
 
--- p1/practice-for-ch-ruby-memory-long-practice-main/card.rb
+++ p1/practice-for-ch-ruby-memory-long-practice-main/card.rb
@@ -1,34 +1,46 @@
 class Card 
-  attr_reader :face_down, :face_value
-  def initialize(face_value) 
-    @face_value = face_value
-    @face_down = true 
-    @default_value = " "
-    @shown_value = @default_value
-  end    
-
-  def card_information 
-    if self.face_down 
-      puts 'card is face down.' 
-      self.default_value
-    else  
-      self.face_value    
-    end  
-  end   
-
-  def hide 
-    if !self.face_down
-      self.face_down = true 
-      @shown_value = @default_value
+    attr_reader :face_down, :face_value, : shown_value
+    def intialize(face_value) 
+        @face_value = face_value
+        @face_down = true 
+        @default_value = " "
+        @shown_value = @default_value
     end    
-  end
 
-  def reveal 
-    if self.face_down
-      puts self.face_value
-      @shown_value = @face_value
-      self.face_down = false 
+    def card_information 
+        if self.face_down 
+            puts 'card is face down.' 
+            self.default_value
+        else  
+            self.face_value    
+        end  
+    end   
+
+    def hide 
+        if !self.face_down
+            self.face_down = true 
+            @shown_value = @default_value
+        end    
+    end
+
+    def reveal 
+        if self.face_down
+            puts self.face_value
+            @shown_value = @face_value
+            self.face_down = false 
+        end    
     end    
-  end    
+
+    def to_s 
+      if !face_down
+        return @face_value
+      else  
+        return @default_value
+      end  
+    end
+    
+    def ==
+
+    end    
 
 end   
