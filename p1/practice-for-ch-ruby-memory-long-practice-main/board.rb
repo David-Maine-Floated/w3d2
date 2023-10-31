43cee176689be629d17c5c800a5bc64aa1668303
csmanel <csmanel@cube>
2023-10-31 15:05:58 -0400

    add board functionality

--- p1/practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ p1/practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -1,7 +1,34 @@
 class Board 
 
-    def intialize()
+  attr_reader :grid
+
+  @@elements = ("a".."h").to_a
+
+  def intialize
+    @grid = Array.new(4) { Array.new(4, nil) }
+    @size = @grid.length / 2
+    #@grid should take in cards as an arg
+  end
+  #populate board with @board,size / 2 , end up with 2 of each card 
+
+  def populate
+    new_arr = @@elements + @@elements
+    
+    while new_arr.length > 0
+      i_one = rand(0...@grid.length)
+      i_two = rand(0...@grid.length)
+      
+      if @grid[i_one][i_two] == nil
+        curr_el = new_arr.pop
+        @grid[i_one][i_two] = Card.new(curr_el)
+      end
 
     end
+  end
+
+  def print
+    puts @grid.each {|row| puts row.join(" ")}
+  end
+
 
 end 
